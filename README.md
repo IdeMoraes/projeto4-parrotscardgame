6e5b27423e22fc668c8424d1d6179f49a88d4458
Igor de Moraes <r.de.moraes.igor@gmail.com>
2023-04-19 21:57:21 -0300

    Code improvements: added README and new game features

---
+++ README.md
@@ -0,0 +1,7 @@
+Bem-vindo ao Parrot Card Game! Este é um jogo da memória super divertido e envolvente, com gifs de papagaios dançantes, efeitos e transições.
+
+![Screenshot do Projeto](./assets/screenshot.png)
+
+O jogo foi desenvolvido utilizando as linguagens de marcação HTML, estilização CSS e programação JavaScript.
+
+Este projeto foi criado durante a formação Full Stack da Driven, onde aprendi a desenvolver aplicativos e páginas web completas, do front-end ao back-end. Espero que você se divirta jogando!
--- scripts.js
+++ scripts.js
@@ -1,26 +1,61 @@
-let quantasCartas = prompt("Digite o número de cartas (par de 4 a 14)");
-
-while(quantasCartas%2!=0||quantasCartas<4||quantasCartas>14){quantasCartas = prompt("Digite o número de cartas (par de 4 a 14)");}
-//alert("Agora sim");//
-
-let minhaArray=['bobrossparrot','bobrossparrot','explodyparrot','explodyparrot','fiestaparrot','fiestaparrot','metalparrot','metalparrot','revertitparrot','revertitparrot','tripletsparrot','tripletsparrot','unicornparrot','unicornparrot']
-let contador1=0;
-while (contador1<14-quantasCartas){minhaArray.pop();contador1++;}
-minhaArray.sort(comparador);
-function comparador() {return Math.random() - 0.5;}
-
-let contador2=1;
-let contador3=0
-while(contador2<=quantasCartas){
-    let elemento1 = document.querySelector(".carta"+contador2+" .theback");
-    elemento1.innerHTML = `<img src='assets/${minhaArray[contador3]}.gif'>`;
-    let elemento2 = document.querySelector(".container"+contador2);
-    elemento2.classList.toggle("containerOn");
-    contador2++;
-    contador3++;
+let numberOfCards = prompt("Digite o número de cartas (par de 4 a 14)");
+while(numberOfCards%2!=0||numberOfCards<4||numberOfCards>14){
+    numberOfCards = prompt("Digite o número de cartas (par de 4 a 14)");
 }
-
-
-function myFlipFunction(classecarta) {
-    const elemento = document.querySelector(classecarta);
-    elemento.classList.toggle("thecardselecionado");}
+let myArray=['bobrossparrot','bobrossparrot','explodyparrot','explodyparrot','fiestaparrot','fiestaparrot','metalparrot','metalparrot','revertitparrot','revertitparrot','tripletsparrot','tripletsparrot','unicornparrot','unicornparrot']
+let i=0;
+while(i<14-numberOfCards){
+    myArray.pop();
+    i++;
+}
+myArray.sort(comparador);
+function comparador(){
+    return Math.random() - 0.5;
+}
+let cardNumber=1;
+while(cardNumber<=numberOfCards){
+    let card = document.querySelector(".carta"+cardNumber+" .theback");
+    card.innerHTML = `<img src='assets/${myArray[cardNumber-1]}.gif'>`;
+    let container = document.querySelector(".container"+cardNumber);
+    container.classList.toggle("containerOn");
+    cardNumber++;
+}
+let sourceCheck = '';
+let attempts = 0;
+let matches = 0;
+function myFlipFunction(cardClass) {
+    const thisCard = document.querySelector(cardClass);
+    const thisCardSource = thisCard.children[1].children[0].src;
+    if(sourceCheck===''){
+        sourceCheck = thisCardSource;
+        thisCard.classList.toggle("selectedCard");
+    } else if(sourceCheck===thisCardSource){
+        sourceCheck = '';
+        thisCard.classList.toggle("selectedCard");
+        const selectedCards = document.querySelectorAll(".selectedCard");
+        selectedCards[1].classList.toggle("matchedCards");
+        selectedCards[0].classList.toggle("matchedCards");
+        selectedCards[1].classList.toggle("selectedCard");
+        selectedCards[0].classList.toggle("selectedCard");
+        attempts++;
+        matches++;
+    } else{
+        sourceCheck = '';
+        thisCard.classList.toggle("selectedCard");
+        const selectedCards = document.querySelectorAll(".selectedCard");
+        setTimeout(() => {
+            selectedCards[1].classList.toggle("selectedCard");
+            selectedCards[0].classList.toggle("selectedCard");
+        }, "1000");
+        attempts++;
+    };
+    if(matches===numberOfCards/2){
+        setTimeout(() => {
+            alert(`Você ganhou em ${attempts} jogadas!`);
+            let restart = confirm("Gostaria de reiniciar a partida?");
+            if(restart){
+                location.reload();
+            }
+        }, "1000");
+    }
+}
